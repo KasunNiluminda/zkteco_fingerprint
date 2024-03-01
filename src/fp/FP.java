82eb7cdbb888ff5b298b0cf0029dcbc0419d27d5
kasun_niluminda <kasunniluminda@gmail.com>
2024-03-02 00:55:16 +0530

    FInger print project added

---
+++ src/fp/FP.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fp;
+
+import com.zkteco.biometric.ZKFPDemo;
+
+/**
+ *
+ * @author NC
+ */
+public class FP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ZKFPDemo demo = new ZKFPDemo();
+        demo.launchFrame();
+
+    }
+
+}
